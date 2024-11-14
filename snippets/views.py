6f07504be65f99dbb7d2f850e81d095ea1489655
chen weichen <newsforcwc@gmail.com>
2024-11-14 16:25:41 +0800

    add relationships and hyperlinkes between entities

--- snippets/views.py
+++ snippets/views.py
@@ -3,12 +3,21 @@ from rest_framework.views import APIView
 from .models import Snippet
 from .serializers import SnippetSerializer, UserSerializer
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import renderers
 from django.http import Http404
 from rest_framework import generics, permissions
 from django.contrib.auth.models import User
 from snippets.permissions import IsOwnerOrReadOnly
+from rest_framework.reverse import reverse
 
+
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'users': reverse('user-list', request=request, format=format),
+        'snippets': reverse('snippet-list', request=request, format=format)
+    })
 
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
@@ -25,6 +34,14 @@ class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
         permissions.IsAuthenticatedOrReadOnly, 
         IsOwnerOrReadOnly
     ]
+
+class SnippetHighlight(generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    renderer_classes = [renderers.StaticHTMLRenderer]
+
+    def get(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
 
 class UserList(generics.ListAPIView):
     queryset = User.objects.all()
